a732777e1252f189c9455a9e5ce63606cc1d738e
Shaamala <shaamalajabbar@gmail.com>
2018-03-06 09:10:52 -0500

    first commit

---
+++ src/com/gc/model/Customers.java
@@ -0,0 +1,18 @@
+package com.gc.model;
+
+/*
+ * @Abduljabbar Shaamala
+ */
+
+public class Customers {
+	private int customerID;
+	private String customerName;
+	private String address;
+	private String phoneNum;
+	
+	public Customers() {
+		
+	}
+
+	
+}
