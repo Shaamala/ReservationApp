6ef107388179b269709f00a05de4021b31f85a43
Shaamala <shaamalajabbar@gmail.com>
2018-03-06 11:37:42 -0500

    customers class variables update

--- src/com/gc/model/Customers.java
+++ src/com/gc/model/Customers.java
@@ -6,9 +6,16 @@ package com.gc.model;
 
 public class Customers {
 	private int customerID;
-	private String customerName;
-	private String address;
-	private String phoneNum;
+	private String fName;
+	private String lName;
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	private String homeTel;
+	private String emrgTel;
+	private String vetName;
+	private String vetTel;
 	
 	public Customers() {
 		
