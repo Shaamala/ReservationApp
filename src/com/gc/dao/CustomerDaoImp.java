9261da2a187c177b626bca0dfd8a574fbc9f4d4b
Shaamala <shaamalajabbar@gmail.com>
2018-03-06 14:28:25 -0500

    add, delete and list customer

--- src/com/gc/dao/CustomerDaoImp.java
+++ src/com/gc/dao/CustomerDaoImp.java
@@ -2,30 +2,78 @@ package com.gc.dao;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import com.gc.model.Customers;
+import com.gc.util.HibernateUtil;
+
 /*
  * @Abduljabbar Shaamala
  */
-public class CustomerDaoImp implements CustomersDao  {
+public abstract class CustomerDaoImp implements CustomersDao {
 	List<Customers> customerList;
 
-	  public List<Customers> readCustomers() {
-		  
-	        Session session = factory.openSession();
-	        Transaction tx = null;
-	        List<Customers> customerList = null;
-	        try {
-	            tx = session.beginTransaction();
-	            customerList = session.createQuery("FROM customers").list();
-	            tx.commit();  //COMMIT MUST COME AFTER THE ACTION
-	        } catch (HibernateException e) {
-	            if (tx != null) tx.rollback();
-	            e.printStackTrace();
-	        } finally {
-	            session.close();
-	        }
-	        return customerList;
+	// display all customers list
+	public List<Customers> readCustomers() {
+
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+		Session session = sessionFactory.openSession();
+		Transaction tx = null;
+		List<Customers> customerList = null;
+		try {
+			tx = session.beginTransaction();
+			customerList = session.createQuery("FROM customers").list();
+			tx.commit(); // COMMIT MUST COME AFTER THE ACTION
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return customerList;
 	}
-	
+
+	// Create a customer in the database
+	public void addUser(Customers customer) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			session.save(customer);
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+
+	public void deleteCustomer(int customerID) {
+
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction tx = null;
+
+		Customers customer = getCustomer(customerID);
+		try {
+			tx = session.beginTransaction();
+			session.delete(customer);
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
 
 }
