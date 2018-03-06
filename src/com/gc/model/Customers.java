1b728ce1d74e8e00566c66d28fe85fa7335c0231
Shaamala <shaamalajabbar@gmail.com>
2018-03-06 11:49:43 -0500

    add constructor and getter and setter

--- src/com/gc/model/Customers.java
+++ src/com/gc/model/Customers.java
@@ -12,6 +12,7 @@ public class Customers {
 	private String city;
 	private String state;
 	private String zip;
+	private String email;
 	private String homeTel;
 	private String emrgTel;
 	private String vetName;
@@ -20,6 +21,122 @@ public class Customers {
 	public Customers() {
 		
 	}
+	
+
+	public Customers(int customerID, String fName, String lName, String street, String city, String state, String zip,
+			String email, String homeTel, String emrgTel, String vetName, String vetTel) {
+		super();
+		this.customerID = customerID;
+		this.fName = fName;
+		this.lName = lName;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.email = email;
+		this.homeTel = homeTel;
+		this.emrgTel = emrgTel;
+		this.vetName = vetName;
+		this.vetTel = vetTel;
+	}
+
+
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getHomeTel() {
+		return homeTel;
+	}
+
+	public void setHomeTel(String homeTel) {
+		this.homeTel = homeTel;
+	}
+
+	public String getEmrgTel() {
+		return emrgTel;
+	}
+
+	public void setEmrgTel(String emrgTel) {
+		this.emrgTel = emrgTel;
+	}
+
+	public String getVetName() {
+		return vetName;
+	}
+
+	public void setVetName(String vetName) {
+		this.vetName = vetName;
+	}
+
+	public String getVetTel() {
+		return vetTel;
+	}
+
+	public void setVetTel(String vetTel) {
+		this.vetTel = vetTel;
+	}
+	
 
 	
 }
--- src/com/gc/model/Dogs.java
+++ src/com/gc/model/Dogs.java
@@ -1,5 +1,76 @@
 package com.gc.model;
 
 public class Dogs {
+	private int dogID;
+	private String dogName;
+	private String breed;
+	private String size;
+	private String food;
+	private String customerID;
+	
+	public Dogs() {
+		
+	}
+
+	public Dogs(int dogID, String dogName, String breed, String size, String food, String customerID) {
+		super();
+		this.dogID = dogID;
+		this.dogName = dogName;
+		this.breed = breed;
+		this.size = size;
+		this.food = food;
+		this.customerID = customerID;
+	}
+
+	public int getDogID() {
+		return dogID;
+	}
+
+	public void setDogID(int dogID) {
+		this.dogID = dogID;
+	}
+
+	public String getDogName() {
+		return dogName;
+	}
+
+	public void setDogName(String dogName) {
+		this.dogName = dogName;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getFood() {
+		return food;
+	}
+
+	public void setFood(String food) {
+		this.food = food;
+	}
+
+	public String getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(String customerID) {
+		this.customerID = customerID;
+	}
+	
+	
+	
 
 }
